00f54dbebc05c9843b9eb02d5afaea29fc91c437
Timofey Kafanov <tim.kafanov@gmail.com>
2021-10-06 09:23:16 +0300

    rename modificator

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 const form = document.querySelector('.popup__form')
-const nameField = document.querySelector('.popup__input_name');
-const postField = document.querySelector('.popup__input_post');
+const nameField = document.querySelector('.popup__input_type_name');
+const postField = document.querySelector('.popup__input_type_post');
 const profileName = document.querySelector('.profile__name');
 const profilePost = document.querySelector('.profile__post');
 
