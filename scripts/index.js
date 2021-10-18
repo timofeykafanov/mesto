2ff689ceb514acc989f8f98b5213e098c9785340
Timofey Kafanov <tim.kafanov@gmail.com>
2021-10-18 17:51:51 +0300

    feat: добаить попап Новое место

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,95 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+]; 
+
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const popupContent = popup.querySelector('.popup__content')
-const popupCloseButton = popupContent.querySelector('.popup__close');
-const form = popupContent.querySelector('.popup__form')
-const nameField = form.querySelector('.popup__input_type_name');
-const postField = form.querySelector('.popup__input_type_post');
+const addButton = document.querySelector('.profile__add-button');
+const editPopup = document.querySelector('.edit-popup');
+const addPopup = document.querySelector('.add-popup');
+const editPopupContent = editPopup.querySelector('.edit-popup__content');
+const addPopupContent = addPopup.querySelector('.add-popup__content');
+const editPopupCloseButton = editPopupContent.querySelector('.edit-popup__close');
+const addPopupCloseButton = addPopupContent.querySelector('.add-popup__close');
+const editForm = editPopupContent.querySelector('.edit-popup__form');
+const addForm = addPopupContent.querySelector('.add-popup__form');
+const nameField = editForm.querySelector('.edit-popup__input_type_name');
+const postField = editForm.querySelector('.edit-popup__input_type_post');
+const placeField = addForm.querySelector('.add-popup__input_type_name');
+const linkField = addForm.querySelector('.add-popup__input_type_post');
 const profileName = document.querySelector('.profile__name');
 const profilePost = document.querySelector('.profile__post');
+const placeName = document.querySelector('.element__title');
+const imageLink = document.querySelector('.element__image');
 
-function openPopup() {
+
+function openEditPopup() {
     nameField.setAttribute('value', profileName.textContent);
     postField.setAttribute('value', profilePost.textContent);
 
-    popup.classList.add('popup_isOpen');
+    editPopup.classList.add('edit-popup_isOpen');
 }
 
-function closePopup() {
-    popup.classList.remove('popup_isOpen');
+function closeEditPopup() {
+    editPopup.classList.remove('edit-popup_isOpen');
 }
 
-editButton.addEventListener('click', openPopup);
+function openAddPopup() {
+    addPopup.classList.add('add-popup_isOpen');
+}
 
-popupCloseButton.addEventListener('click', closePopup);
+function closeAddPopup() {
+    addPopup.classList.remove('add-popup_isOpen');
+}
 
-function submitForm(event) {
+editButton.addEventListener('click', openEditPopup);
+
+editPopupCloseButton.addEventListener('click', closeEditPopup);
+
+addButton.addEventListener('click', openAddPopup);
+
+addPopupCloseButton.addEventListener('click', closeAddPopup)
+
+function submitEditForm(event) {
     event.preventDefault()
 
     profileName.textContent = nameField.value;
     profilePost.textContent = postField.value;
 
-    closePopup();
+    closeEditPopup();
 }
 
-form.addEventListener('submit', submitForm);
+editForm.addEventListener('submit', submitEditForm);
 
 function popupClickHandler(event) {
-    if (event.target.classList.contains('popup')) {
-        closePopup()
+    if (event.target.classList.contains('edit-popup')) {
+        closeEditPopup()
+    } else if (event.target.classList.contains('add-popup')) {
+        closeAddPopup()
     }
 }
 
-popup.addEventListener('mouseup', popupClickHandler)
+editPopup.addEventListener('mouseup', popupClickHandler);
+addPopup.addEventListener('mouseup', popupClickHandler);
